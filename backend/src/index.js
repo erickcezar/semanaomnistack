8660deb458198085130ce843a0612f53cad8d942
Erick Oliveira <erickcezar.oliveira@gmail.com>
2020-01-14 12:46:04 -0300

    importacao projeto semana omnistack

---
+++ backend/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-xdant.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(routes);
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros
+
+// Query Params: req.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+// MongoDB
+
+app.listen(3333);
